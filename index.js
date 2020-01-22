6f5f1448d08d761e2720546b2b7afbaf6739cfca
Aditya <adityam1311@gmail.com>
2020-01-22 21:59:24 +0530

    Added interval function for 5 minutes

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const market = require('steam-market-pricing');
-
-var data_json = null;
-market.getItemPrice(730, 'Shattered Web Case', 24).then(function (item) {
-  data_json = item;
-  console.log(data_json);
-});
-
-
-
+const api_token = process.env.DISCORD_BOT_TOKEN;
+var previous_max_price = 0.0;
+var ifunc = require('./interval_function');
+const Omega = '\u03A9';
+const rupee = '\u20A8';
 client.once('ready', () => {
   console.log('Up and running!');
 });
 
-client.login('<token>');
+var status = client.login(api_token);
+console.log(status);
 
 client.on('message', message => {
   if (message.content === '!ping') {
@@ -30,3 +27,19 @@ client.on('message', message => {
     });
   }
 });
+
+function execute_interval_function(){
+  var price, status = ifunc.intervalFunction(previous_max_price);
+  if (status == true){
+    client.channels.get(process.env.STEAM_MARKET_CHANNEL_ID).send(Omega + '!Price of Shattered Web Case rose to ' + rupee + ' ' + price);
+  }
+  else{
+    client.channels.get(process.env.STEAM_MARKET_CHANNEL_ID).send(Omega + '--Interval Testing--');
+  }
+}
+
+var minutes = 5
+var seconds = 60 * minutes;
+var milliseconds = 1000 * seconds;
+
+setInterval(execute_interval_function, milliseconds);
